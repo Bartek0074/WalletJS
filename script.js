30a07ef3e4ef737c2ee5dba1efff34edc509264d
Bartek0074 <bartekdo123@gmail.com>
2022-10-13 21:28:20 +0200

    added createNewTransaction, selectCategory, checkCategory functions

--- script.js
+++ script.js
@@ -30,7 +30,7 @@ const hidePanel = () => {
 
 const checkForm = () => {
     if (transactionNameInput.value !== '' && transactionAmountInput.value !== '' && transactionCategoryInput.value !== 'none') {
-        console.log('formularz git')
+        createNewTransaction();
     } else {
         alert('Complete the form!')
     }
@@ -40,6 +40,47 @@ const clearInputs = () => {
     transactionNameInput.value = '';
     transactionAmountInput.value = '';
     transactionCategoryInput.value = 'none';
+}
+
+const createNewTransaction = () => {
+    const newTransaction = document.createElement('div');
+    newTransaction.classList.add('transaction');
+    newTransaction.setAttribute('id', ID);
+
+    checkCategory(selectedCategory);
+
+    newTransaction.innerHTML = `
+    <p class="transaction-name">${categoryIcon}${transactionNameInput.value}</p>
+    <p class="transaction-amount">${transactionAmountInput.value}$<button class="delete" onclick="deleteTransaction(${ID})"><i class="fas fa-times"></i></button></p>
+    `
+
+    transactionAmountInput.value > 0 ? incomeSection.appendChild(newTransaction) && newTransaction.classList.add('income') : expensesSection.appendChild(newTransaction) && newTransaction.classList.add('expense')
+    moneyArr.push(parseFloat(transactionAmountInput.value));
+
+    hidePanel();
+    ID++;
+    clearInputs();
+}
+
+const selectCategory = () => {
+    selectedCategory = transactionCategoryInput.options[transactionCategoryInput.selectedIndex].text;
+} 
+
+const checkCategory = transaction => {
+    switch (transaction) {
+        case `[ + ] Income`:
+            categoryIcon = '<i class="fas fa-money-bill-wave"></i>';
+            break;
+        case `[ - ] Shopping`:
+            categoryIcon = '<i class="fas fa-cart-arrow-down"></i>';
+            break;
+        case `[ - ] Food`:
+            categoryIcon = '<i class="fas fa-hamburger"></i>';
+            break;
+        case `[ - ] Entertainment`:
+            categoryIcon = '<i class="fas fa-film"></i>';
+            break;
+    }
 }
 
 addTransactionBtn.addEventListener('click', showPanel);
